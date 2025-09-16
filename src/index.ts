1d090d6bd394fa008a5fd939a18596fc67d7eee1
GemmaRyan <s00250864@atu.ie>
2025-09-16 15:54:56 +0100

    Finsihed Sheet2

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, {Application, Request, Response} from "express" ;
 import morgan from "morgan";
+import userRoutes from './routes/users';
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,10 +12,14 @@ app.get("/ping", async (_req : Request, res: Response) => {
     });
 });
 
+app.use(morgan("tiny"));
+
+
+app.use('/api/v1/users', userRoutes)
+app.use(express.json());
+
 app.listen(PORT, () => {
     console.log("Server is running on port", PORT);
     });
 app.get('/tequila', async (_req : Request, res: Response) => 
     res.send('Tequila time!'));
-
-app.use(morgan("tiny"));
